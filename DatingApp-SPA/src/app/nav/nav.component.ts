b947eb3893111f4228a55de5c312903b8284fa9a
PhamTrungTruong <trungtruongpham@gmail.com>
2020-07-18 17:33:20 +0700

    add error handling and alertify

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,27 +11,27 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   // tslint:disable-next-line: typedef
   ngOnInit() {
   }
 
-  login(): void{
+  login(): void {
     this.authService.login(this.model).subscribe(next => {
-      console.log('login successfully');
+      this.alertify.success('Logged in successfully');
     }, error => {
-      console.log('failed to login');
+      this.alertify.error(error);
     });
   }
 
-  loggedIn(): boolean{
+  loggedIn(): boolean {
     const token = localStorage.getItem('token');
     return !!token;
   }
 
-  logOut(): void{
+  logOut(): void {
     localStorage.removeItem('token');
-    console.log('log out');
+    this.alertify.success('Logged out');
   }
 }
--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using System.ComponentModel.Design;
 using System;
 using System.Collections.Generic;
@@ -18,6 +19,9 @@ using DatingApp.API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API
 {
@@ -64,6 +68,23 @@ namespace DatingApp.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(builder =>
+                {
+                    builder.Run(async context =>
+                    {
+                        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                        var error = context.Features.Get<IExceptionHandlerFeature>();
+                        if (error != null)
+                        {
+                            context.Response.AddApplicationError(error.Error.Message);
+                            await context.Response.WriteAsync(error.Error.Message);
+                        }
+                    });
+                });
             }
 
             // app.UseHttpsRedirection();
